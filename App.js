71179a0b765795bc70e7f7a8392973e8cb690a83
khenryjoe <joseph.ketterer@gmail.com>
2020-01-22 11:59:14 +0000

    list

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, ScrollView } from "react-native";
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: 'shaun', id: '1' },
-    { name: 'yoshi', id: '2' },
-    { name: 'mario', id: '3' },
-    { name: 'luigi', id: '4' },
-    { name: 'peach', id: '5' },
-    { name: 'toad', id: '6' },
-    { name: 'bowser', id: '7' },
+    { name: "shaun", id: "1" },
+    { name: "yoshi", id: "2" },
+    { name: "mario", id: "3" },
+    { name: "luigi", id: "4" },
+    { name: "peach", id: "5" },
+    { name: "toad", id: "6" },
+    { name: "bowser", id: "7" }
   ]);
 
   return (
     <View style={styles.container}>
-
+      <ScrollView>
+        {people.map(item => (
+          <View key={item.id} style={styles.item}>
+            <Text>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -22,8 +28,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-   
+    backgroundColor: "#fff"
   },
-
+  item: {
+    margin: 30,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24
+  }
 });
