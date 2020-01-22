f25f435ae1136c1d6348feeb2b4da034ea095159
khenryjoe <joseph.ketterer@gmail.com>
2020-01-22 11:00:51 +0000

    first

--- App.js
+++ App.js
@@ -2,25 +2,19 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('shaun');
-  const [age, setAge] = useState('30');
+  const [people, setPeople] = useState([
+    { name: 'shaun', id: '1' },
+    { name: 'yoshi', id: '2' },
+    { name: 'mario', id: '3' },
+    { name: 'luigi', id: '4' },
+    { name: 'peach', id: '5' },
+    { name: 'toad', id: '6' },
+    { name: 'bowser', id: '7' },
+  ]);
 
   return (
     <View style={styles.container}>
 
-      <Text>Enter name:</Text>
-      <TextInput 
-        placeholder='e.g. John Doe' 
-        style={styles.input}
-        onChangeText={(value) => setName(value)} />
-
-      <Text>Enter age:</Text>
-      <TextInput 
-        placeholder='e.g. 99' 
-        style={styles.input}
-        onChangeText={(value) => setAge(value)} />
-
-      <Text style={styles.result}>name: {name}, age: {age}</Text>
     </View>
   );
 }
@@ -29,14 +23,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-  }
+
 });
